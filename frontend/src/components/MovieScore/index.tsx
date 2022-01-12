87a9032253cd41f5cd832d4b939640b159eb4fe2
leonardo_tolentino29 <leonardoleoat@yahoo.com.br>
2022-01-12 17:34:36 -0300

    MovieCard

---
+++ frontend/src/components/MovieScore/index.tsx
@@ -0,0 +1,18 @@
+import './style.css';
+import MovieStars from '../MovieStars';
+
+function MovieScore(){
+
+  const score = 3.8;
+  const count = 13;
+
+  return(
+    <div className="dsmovie-score-container">
+    <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+    <MovieStars />
+    <p className="dsmovie-score-count">{count} avaliações</p>
+</div>
+  );
+}
+
+export default MovieScore;
